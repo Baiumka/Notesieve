b5a634464368e1349640aa2930095f99d0d939f0
Baiumka <igrulenka@gmail.com>
2021-06-03 22:16:34 +0300

    Updater

---
+++ NotesieveUpdater/NotesieveUpdater/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace NotesieveUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			Process[] procs = Process.GetProcessesByName("Notesieve");
+			foreach (Process p in procs)
+			{
+				p.Kill();
+				Thread.Sleep(1000);
+			}
+
+			string targetDirectory = Environment.CurrentDirectory + @"\" + "Updates";
+			if (!Directory.Exists(targetDirectory)) return;
+
+			string[] fileEntries = Directory.GetFiles(targetDirectory);
+			foreach (string oldFile in fileEntries)
+			{
+				Console.WriteLine(oldFile);
+				string fileName = Path.GetFileName(oldFile);
+				string newFile = Environment.CurrentDirectory + @"\" + fileName;
+				if (File.Exists(newFile))
+				{
+					File.Delete(newFile);
+				}
+				File.Move(oldFile, newFile);
+			}
+
+			Directory.Delete(targetDirectory);
+
+			Process.Start(Environment.CurrentDirectory + @"/" + "Notesieve.exe");			
+		}
+    }
+}
